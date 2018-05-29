095c244b55d676312e11e12136d87b52df33d71e
vacuole1989 <154034117@qq.com>
2018-05-30 07:03:47 +0800

    3123

--- EngRoomServer/src/main/java/com/cxd/engroom/App.java
+++ EngRoomServer/src/main/java/com/cxd/engroom/App.java
@@ -1,28 +1,63 @@
 package com.cxd.engroom;
 
-import com.cxd.engroom.util.WxMessageConverter;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author ws03089
  */
+
 @SpringBootApplication
+@EntityScan("com.cxd.engroom.bean")
+@MapperScan("com.cxd.engroom.mapper")
 public class App extends SpringBootServletInitializer {
 
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new WxMessageConverter());
+        restTemplate.getMessageConverters().add(getMappingJackson2HttpMessageConverterJson());
         return restTemplate;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource(){
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        Configuration configuration=new Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sqlSessionFactoryBean.setConfiguration(configuration);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/com/cxd/rtcroom/mapper/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 
     @Override
@@ -31,12 +66,20 @@ public class App extends SpringBootServletInitializer {
     }
 
     @Bean
-    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverterJson() {
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-        //设置中文编码格式
-        List<MediaType> list = new ArrayList<>();
-        list.add(MediaType.APPLICATION_JSON_UTF8);
-        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        mediaTypes.add(MediaType.TEXT_PLAIN);
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(mediaTypes);
+        return mappingJackson2HttpMessageConverter;
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverterText() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.TEXT_PLAIN);
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(mediaTypes);
         return mappingJackson2HttpMessageConverter;
     }
 
