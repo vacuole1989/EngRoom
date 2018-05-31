37071aa681fb635744fd01a6257b840c877a4254
vacuole1989 <154034117@qq.com>
2018-05-31 15:56:23 +0800

    1

---
+++ EngRoomServer/src/main/java/com/cxd/engroom/bean/AppTag.java
@@ -0,0 +1,42 @@
+package com.cxd.engroom.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+public class AppTag implements Serializable {
+    @Id
+    private String appId;
+    @NotNull
+    private String appSecret;
+    private String appTemplate;
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public AppTag setAppId(String appId) {
+        this.appId = appId;
+        return this;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public AppTag setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+        return this;
+    }
+
+    public String getAppTemplate() {
+        return appTemplate;
+    }
+
+    public AppTag setAppTemplate(String appTemplate) {
+        this.appTemplate = appTemplate;
+        return this;
+    }
+}
